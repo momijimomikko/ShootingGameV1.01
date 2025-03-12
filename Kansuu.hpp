d99f5696093732592e38ba7e0e1e58d2e75cc35e
momijimomikko <zongtaitengyuan38@gmail.com>
2025-03-12 10:19:02 +0900

    Update Kansuu.hpp
    ヘッダファイルに関数の書き込み

--- Kansuu.hpp
+++ Kansuu.hpp
@@ -1,1 +1,30 @@
 
+
+
+
+int Windowsetup(void);
+int Change(void);
+int Explane(void);
+int Homehome(void);
+int Home(void);
+int LoadImg(void);
+int me(void); 
+int Ball(void);
+int Timer(void);
+int MyHantei(void);
+int HPbar(void);
+int scoreDisplay(void);
+int LoadImg(void);
+int Hantei1(void);
+int Hantei2(void);
+int Hantei3(void);
+int Hantei4(void);
+int Hantei5(void);
+int Hantei6(void);
+int Hantei7(void);
+int Display(void);
+int Kougeki1(void);
+int MainTimerCount(void);
+int window(void);
+int Endwindow1(void);
+int Endwindow2(void);
